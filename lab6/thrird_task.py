9d1f21bedb9caeb32c4fc22067a4c3057509b640
Aryukhin <sashka.rf@mail.ru>
2024-02-13 11:41:58 +0300

    Финальная версия с многопоточностью, многопроцессорностью и без всего

--- lab6/thrird_task.py
+++ lab6/thrird_task.py
@@ -1,37 +1,53 @@
 # 2 var
 import numpy as np
 import time
-from concurrent.futures import ProcessPoolExecutor
-
-
-start = time.perf_counter()
-
-P = np.random.randint(-10, 10, 5000)
-Q = np.random.randint(-10, 10, 5000)
+from concurrent.futures import ProcessPoolExecutor, ThreadPoolExecutor
 
 
 def matr(N):
-    x = []
+    P = np.random.randint(-10, 10, N)
+    Q = np.random.randint(-10, 10, N)
+
     matrix = []
     for i in range(N):
+        row = []
         for j in range(N):
             r = 1 / (1 + (Q[j] - P[i]) ** 2)
-            x.append(r)
+            row.append(r)
+        matrix.append(row)
+    return matrix
 
-        matrix.append(x)
-        x = []
+
+if __name__ == "__main__":
+    start = time.perf_counter()
+    # Использование многопроцессорности
+
+    # with ProcessPoolExecutor(max_workers=None) as executor:
+    #     future = executor.submit(matr, 5000)
+    #     result = future.result()
 
 
-# matix = matr(5000)
+    # Базовый вызов
 
-with ProcessPoolExecutor(max_workers=None) as executor:
-    # Запуск задачи параллельно
-    future = executor.submit(matr, 5000)
-    # Ожидание результата и получение его
-    # result = future.result()
+    # result = matr(5000)
 
-finish = time.perf_counter()
+    # Использование многопоточности
 
-print(f'The time is {finish - start}')
+    with ThreadPoolExecutor(max_workers=None) as executor:
+        future = executor.submit(matr, 5000)
+        result = future.result()
 
-# The time is 26.6003019
+    finish = time.perf_counter()
+
+    print(f'The time is {finish - start}')
+
+# Без использования многопроцессорности
+# The time is 17.7919322
+
+
+# С использованием многопроцессорности
+# The time is 217.9143072
+
+
+# С использованием многопоточности
+# The time is 16.9122773
